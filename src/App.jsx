6122f85fddf71687eb8f97231fadb090c87ecba7
hanushgowrav66 <hanushgowrav66@gmail.com>
2025-01-30 20:34:01 -0500

    Contact Added  version

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Gallery from "./components/Gallery";
 import ArtworkDetail from "./components/ArtworkDetail";
 import Contact from "./components/Contact";
-import TitlebarBelowMasonryImageList from "./components/TitlebarBelowMasonryImageList";
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery/:category" element={<Gallery />} />
-        <Route path="/title" element={<TitlebarBelowMasonryImageList />} />
         <Route path="/artwork/:id" element={<ArtworkDetail />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
